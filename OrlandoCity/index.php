ffd3ebf529fe2f36dde56aa73c4007ad9aa0ef7e
Julio <julio.silva@virtus.ufcg.edu.br>
2017-01-24 16:01:19 -0300

    Php configuration, comunication between php and mysql, reuse header e footer

---
+++ OrlandoCity/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'inc/Slim-2.x/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+// GET route
+$app->get(
+    '/',
+    function () {
+        include_once("view/index.php");
+    }
+);
+
+$app->get(
+    '/videos',
+    function () {
+        include_once("view/videos.php");
+    }
+);
+
+$app->run();
---
+++ OrlandoCity/view/index.php
@@ -0,0 +1,116 @@
+<?php include_once("header.php"); ?>
+
+<section>
+	<div id="banner">
+		<h1>Orlando City<small>Orlando City Soccer Club</small></h1>
+	</div>
+
+	<div id="news" class="container">
+		<div  class="row text-center">
+			<h2>Latest News</h2>
+			<hr>
+		</div>
+
+		<button type="button" id="btn-news-prev" class="fa fa-angle-left"></button>
+		<button type="button" id="btn-news-next" class="fa fa-angle-right"></button>
+
+		<div class="row thumbnails">				
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>
+
+			<div class="item">
+				<div class="item-inner">
+					<img src="img/noticia-thumb.jpg" alt="Noticia">
+					<h3>Orlando City Acquires Goalkeeper Joe Bandik from Toronto FC</h3>
+					<time>December 21,2015</time>
+				</div>
+			</div>	
+		</div>
+	</div>
+
+	<div id="estatisticas">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<p>61348<small>Stadium Capacity</small></p>
+				</div>
+				<div class="col-md-4">
+					<p>2010<small>Founded</small></p>
+				</div>
+				<div class="col-md-4">
+					<p>7th<small>Eastern Conference</small></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="call-to-action">
+		<div class="container">
+			<div  class="row text-center">
+				<h2>American club number one in Brazil</h2>
+				<hr>
+			</div>
+
+			<div class="row">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+
+			<div class="row row-max-400">
+				<div class="col-xs-6">
+					<a href="#" class="btn btn-roxo">Shop</a>
+				</div>
+
+				<div class="col-xs-6">
+					<a href="#" class="btn btn-amarelo">Register</a>
+				</div>
+			</div>
+		</div>
+	</div>		
+</section>
+
+<?php include_once("footer.php"); ?>
