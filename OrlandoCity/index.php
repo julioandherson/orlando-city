51103265419d7671b35d242682fb7d14eced8e83
Julio <julio.silva@virtus.ufcg.edu.br>
2017-02-07 09:58:37 -0300

    Add and Remove of Cart

--- OrlandoCity/index.php
+++ OrlandoCity/index.php
@@ -11,47 +11,52 @@ $app = new \Slim\Slim();
 $app->get(
     '/',
     function () {
-        include_once("view/index.php");
+
+        require_once("view/index.php");
+        
     }
 );
 
 $app->get(
     '/videos',
     function () {
-        include_once("view/videos.php");
+        
+        require_once("view/videos.php");
+        
     }
 );
 
 $app->get(
     '/shop',
     function () {
-        include_once("view/shop.php");
+        
+        require_once("view/shop.php");
+        
     }
 );
 
-$app->get("/produtos", function() {
+$app->get('/produtos', function(){
+
     $sql = new Sql();
 
-    $data = $sql->select(
-                "SELECT * FROM tb_produtos
-                WHERE preco_promorcional > 0 
-                ORDER BY preco_promorcional DESC  
-                LIMIT 3;");
+    $data = $sql->select("SELECT * FROM tb_produtos where preco_promorcional > 0 order by preco_promorcional desc limit 3;");
 
     foreach ($data as &$produto) {
-        $preco = $produto["preco"];
+        $preco = $produto['preco'];
         $centavos = explode(".", $preco);
-        $produto["preco"] = number_format($preco, 0, ",", ".");
-        $produto["centavos"] = end($centavos);
-        $produto["parcelas"] = 10;
-        $produto["parcela"] = number_format($preco / $produto["parcelas"], 2, ",", ".");
-        $produto["total"] = number_format($preco, 2, ",", ".");
+        $produto['preco'] = number_format($preco, 0, ",", ".");
+        $produto['centavos'] = end($centavos);
+        $produto['parcelas'] = 10;
+        $produto['parcela'] = number_format($preco/$produto['parcelas'], 2, ",", ".");
+        $produto['total'] = number_format($preco, 2, ",", ".");
     }
 
     echo json_encode($data);
+
 });
 
-$app->get("/produtos-mais-buscados", function(){
+$app->get('/produtos-mais-buscados', function(){
+
     $sql = new Sql();
 
     $data = $sql->select("
@@ -91,33 +96,136 @@ $app->get("/produtos-mais-buscados", function(){
     ");
 
     foreach ($data as &$produto) {
-        $preco = $produto["preco"];
+        $preco = $produto['preco'];
         $centavos = explode(".", $preco);
-        $produto["preco"] = number_format($preco, 0, ",", ".");
-        $produto["centavos"] = end($centavos);
-        $produto["parcelas"] = 10;
-        $produto["parcela"] = number_format($preco / $produto["parcelas"], 2, ",", ".");
-        $produto["total"] = number_format($preco, 2, ",", ".");
+        $produto['preco'] = number_format($preco, 0, ",", ".");
+        $produto['centavos'] = end($centavos);
+        $produto['parcelas'] = 10;
+        $produto['parcela'] = number_format($preco/$produto['parcelas'], 2, ",", ".");
+        $produto['total'] = number_format($preco, 2, ",", ".");
     }
+
     echo json_encode($data);
+
 });
 
 $app->get("/produto-:id_prod", function($id_prod){
+
     $sql = new Sql();
 
     $produtos = $sql->select("SELECT * FROM tb_produtos WHERE id_prod = $id_prod");
 
     $produto = $produtos[0];
 
-    $preco = $produto["preco"];
+    $preco = $produto['preco'];
     $centavos = explode(".", $preco);
-    $produto["preco"] = number_format($preco, 0, ",", ".");
-    $produto["centavos"] = end($centavos);
-    $produto["parcelas"] = 10;
-    $produto["parcela"] = number_format($preco / $produto["parcelas"], 2, ",", ".");
-    $produto["total"] = number_format($preco, 2, ",", ".");
+    $produto['preco'] = number_format($preco, 0, ",", ".");
+    $produto['centavos'] = end($centavos);
+    $produto['parcelas'] = 10;
+    $produto['parcela'] = number_format($preco/$produto['parcelas'], 2, ",", ".");
+    $produto['total'] = number_format($preco, 2, ",", ".");
 
     require_once("view/shop-produto.php");
+
+});
+
+$app->get(
+    '/cart',
+    function () {
+        
+        require_once("view/cart.php");
+        
+    }
+);
+
+$app->get('/carrinho-dados', function(){
+
+    $sql = new Sql();
+
+    $result = $sql->select("CALL sp_carrinhos_get('".session_id()."')");
+
+    $carrinho = $result[0];
+
+    $sql = new Sql();
+
+    $carrinho['produtos'] = $sql->select("CALL sp_carrinhosprodutos_list(".$carrinho['id_car'].")");
+
+    $carrinho['total_car'] = number_format((float)$carrinho['total_car'], 2, ',', '.');
+    $carrinho['subtotal_car'] = number_format((float)$carrinho['subtotal_car'], 2, ',', '.');
+    $carrinho['frete_car'] = number_format((float)$carrinho['frete_car'], 2, ',', '.');
+
+    echo json_encode($carrinho);
+
+});
+
+$app->get('/carrinhoAdd-:id_prod', function($id_prod){
+
+    $sql = new Sql();
+
+    $result = $sql->select("CALL sp_carrinhos_get('".session_id()."')");
+
+    $carrinho = $result[0];
+
+    $sql = new Sql();
+
+    $sql->query("CALL sp_carrinhosprodutos_add(".$carrinho['id_car'].", ".$id_prod.")");
+
+    header("location: cart");
+    exit;
+
+});
+
+$app->delete("/carrinhoRemoveAll-:id_prod", function($id_prod){
+
+    $sql = new Sql();
+
+    $result = $sql->select("CALL sp_carrinhos_get('".session_id()."')");
+
+    $carrinho = $result[0];
+
+    $sql = new Sql();
+
+    $sql->query("CALL sp_carrinhosprodutostodos_rem(".$carrinho['id_car'].", ".$id_prod.")");
+
+    echo json_encode(array(
+        "success"=>true
+    ));
+});
+
+$app->post("/carrinho-produto", function(){
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $sql = new Sql();
+
+    $result = $sql->select("CALL sp_carrinhos_get('".session_id()."')");
+
+    $carrinho = $result[0];
+
+    $sql = new Sql();
+
+    $sql->query("CALL sp_carrinhosprodutos_add(".$carrinho['id_car'].", ".$data["id_prod"].")");
+
+    echo json_encode(array(
+        "success"=>true
+    ));
+});
+
+$app->delete("/carrinho-produto", function(){
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $sql = new Sql();
+
+    $result = $sql->select("CALL sp_carrinhos_get('".session_id()."')");
+
+    $carrinho = $result[0];
+
+    $sql = new Sql();
+
+    $sql->query("CALL sp_carrinhosprodutos_rem(".$carrinho['id_car'].", ".$data["id_prod"].")");
+
+    echo json_encode(array(
+        "success"=>true
+    ));
 });
 
 $app->run();
